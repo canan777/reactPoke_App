3e485bbd6e56eee290f8e2434095d16e2a327752
canan777 <canan1407@gmail.com>
2024-01-28 16:45:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pokegame from "./Pokegame";
+import "./Pokecard.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pokecard.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    return (
+      <div className="Pokecard">
+        <h3 className="Pokecard-title">{this.props.name}</h3>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name}></img>
+        </div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP : {this.props.exp}</div>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
